56eaf2075deaddd750995d59a5e93e258de3bb52
Nguyen Lam <lpn11127@gmail.com>
2023-03-24 18:49:01 -0500

    New updates for heroku deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l-n%92_23*c*7f5%0v%hc=p8ae#x(jqnabbb0g9a%lr-i6s((%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
