d9d61b102d3481eadc1d57b56a536c7e07082855
Nguyen Lam <lpn11127@gmail.com>
2023-03-24 18:25:35 -0500

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,11 @@
+# pages/views.py
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name = "index.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
